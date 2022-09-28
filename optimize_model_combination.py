7d2381ec9c454094612ab145e3a8d5e671893dce
Joshua Merritt Mitchell <jmmitchell@lanl.gov>
2022-09-28 15:56:37 -0600

    Replace optimize_model_combination.py
    This version uses an L2-norm like utility function and converges faster.

--- optimize_model_combination.py
+++ optimize_model_combination.py
@@ -22,6 +22,7 @@ import time
 # todo - yes those were globals and it needs to be refactored but pygad can't take nested functions
 # todo - so that means you can't have a factory method to produce a properly scoped energy function
 
+
 ga_wrapper = json.load(open(sys.argv[1]))
 query_df = pd.read_pickle(ga_wrapper["query_df_path"])
 reference_df = pd.read_pickle(ga_wrapper["reference_df_path"])
@@ -42,25 +43,35 @@ def find_rank(row):
         return row["orig_rank"]
 
 def energy_function(chromosome, solution_idx):
-    print(chromosome)
-    chromosome = [0 for _ in chromosome]
     t1 = time.time()
-
     query_df["filtered_color_vector"] = query_df["pred_color_vector"].apply(lambda x: np.logical_and(x, chromosome))
     query_df["filtered_color_vector_magnitude"] = [np.sum(x) for x in query_df["filtered_color_vector"]]
     new_ranks = query_df.apply(find_rank, axis=1)
-    energy = np.sum(np.sign(query_df["orig_rank"] - new_ranks))
+    diffs = query_df["orig_rank"] - new_ranks
+    signs = np.sign(diffs)
+    diffs_squared = diffs**2
+    signed_squared_diffs = np.multiply(signs, diffs_squared)
+    energy = np.sum(signed_squared_diffs)
+
+    #energy = np.sum(np.sign(query_df["orig_rank"] - new_ranks))
     t2 = time.time()
-    print(t2 - t1)
     return energy
 
 def on_generation_dump(ga_instance):
     current_time = time.time()
+    print(current_time - start_time, cluster_limit_hours * 60 * 60)
     if current_time - start_time < cluster_limit_hours * 60 * 60:
         print("Total Pop Encountered: ", ga_wrapper["total_pop"], " Total Generations", ga_wrapper["total_generations"])
         best_chromosome, best_fitness, _ = ga_instance.best_solution()
+
+        if "performance_history" in ga_wrapper:
+            ga_wrapper["performance_history"].append(best_fitness)
+        else:
+            ga_wrapper["performance_history"] = [best_fitness]
+
+
         print("\t", best_chromosome, best_fitness)
-        if best_fitness == ga_wrapper["last_best_solution_fitness"] or best_chromosome.tolist() == ga_wrapper["last_best_solution_chromosome"]:
+        if best_fitness > 0 and (best_fitness == ga_wrapper["last_best_solution_fitness"] or best_chromosome.tolist() == ga_wrapper["last_best_solution_chromosome"]):
             ga_wrapper["converged"] += 1
             if ga_wrapper["converged"] == convergence_limit:
                 print("Model Converged!")
@@ -85,9 +96,12 @@ def on_generation_dump(ga_instance):
         ga_wrapper["total_pop"] += ga_instance.sol_per_pop
         ga_wrapper["total_generations"] += 1
         ga_wrapper["previous_population"] = ga_instance.population.tolist()
-        #json.dump(ga_wrapper, open(ga_wrapper["wrapper_path"], 'w+'), indent=4, sort_keys=True)
+        json.dump(ga_wrapper, open(ga_wrapper["wrapper_path"], 'w+'), indent=4, sort_keys=True)
     else:
+        print("call new instance")
+        os.system("sbatch ./optimize_batch.sh " + ga_wrapper["wrapper_path"] + " " + str(cluster_limit_hours) + " " + str(convergence_limit))
         exit()
+
 
 if __name__ == '__main__':
     if ga_wrapper["previous_population"]:
@@ -96,11 +110,14 @@ if __name__ == '__main__':
         previous_population = None
     if ga_wrapper["converged"]:
         warnings.warn("Optimizing Converged Model")
+    if ga_wrapper["total_generations"] >= 1000:
+        print("generation limit met")
+        exit()
     ga_instance = pygad.GA(
-        num_generations=1, #ga_wrapper["num_generations"],
-        num_parents_mating=1,
+        num_generations=1000, #ga_wrapper["num_generations"],
+        num_parents_mating=5,
         fitness_func=energy_function,
-        sol_per_pop=1,
+        sol_per_pop=40,
         num_genes=ga_wrapper["num_genes"],
         gene_type=int,
         init_range_low=ga_wrapper["init_range_low"],
